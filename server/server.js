0364b9206745d9127866d514428a5604017bbce6
Kevin Browne <kbrowne97@gmail.com>
2023-09-20 08:00:56 -0400

    debug logs

--- server/server.js
+++ server/server.js
@@ -87,6 +87,7 @@ app.get("/discord/auth", async (req, res) => {
     }
   );
 
+  console.log(response.headers);
   const userResponse = await axios.get("https://discord.com/api/users/@me", {
     headers: {
       Authorization: `Bearer ${response.data.access_token}`,
